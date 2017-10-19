cfe832c70bebf739641ee9a9a3be2829dd8db2e4
MuZhou <MuZhou233@outlook.com>
2017-10-19 18:54:49 +0800

    add new texiao

---
+++ js/muzhou.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$("button").click(function(jumpevent){
+		var jumpurl = $(this).attr("href");
+		if(typeof jumpurl != "undefined" && jumpurl[0] != '#'){
+			jumpevent.preventDefault();
+			$("#loading").fadeIn("fast");
+			window.setTimeout(function(){
+				window.location.href=jumpurl,500
+			});
+		}
+	});
+	$("a").click(function(jumpevent){
+		var jumpurl = $(this).attr("href");
+		if(typeof jumpurl != "undefined" && jumpurl[0] != '#'){
+			jumpevent.preventDefault();
+			$("#loading").fadeIn("fast");
+			window.setTimeout(function(){
+				window.location.href=jumpurl,500
+			});
+		}
+	});
+});
